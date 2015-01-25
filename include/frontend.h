67dbd025055afd9fef665b9a474b281a62c0811c
florek <florrek@florkiewicz.me>
2015-01-25 13:01:39 +0100

    added headers for real

---
+++ include/frontend.h
@@ -0,0 +1,8 @@
+
+int8_t record_compare_name(void* lhs, void* rhs);
+int8_t record_compare_computername(void* lhs, void* rhs);
+int8_t record_compare_surname(void* lhs, void* rhs);
+int8_t record_compare_workspace(void* lhs, void* rhs);
+
+void insert_all(record* r);
+void delete_all(record* r);
